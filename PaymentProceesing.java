846647d61df286666bd8be71fa116a8d65142be4
Garima Varshney <141229671+Garimajii@users.noreply.github.com>
2024-07-19 22:56:53 +0530

    Add files via upload

---
+++ PaymentProceesing.java
@@ -0,0 +1,9 @@
+package mypack;
+
+public class PaymentProceesing {
+ public void processPayment(Reservation reservation) {
+             System.out.println("Rooms not available payment not done");
+    
+ }
+}
+
